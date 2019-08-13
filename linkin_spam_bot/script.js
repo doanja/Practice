2023b3f138e4612cb9e332a8c60f11c916e0ef34
John Doan <doanja@plu.edu>
2019-08-13 08:46:13 -0700

    Added Linkin Connection script

---
+++ linkin_spam_bot/script.js
@@ -0,0 +1,11 @@
+// run the addConnection() function in the console at https://www.linkedin.com/mynetwork/
+const addConnection = () => {
+    // retrieves all buttons into an array
+    const a = document.getElementsByClassName('js-discover-person-card__action-btn full-width artdeco-button artdeco-button--2 artdeco-button--full artdeco-button--secondary ember-view');
+
+    // loop through all the buttons and call click()
+    for ( let i = 0; i < a.length; i++){
+        a[i].click();
+    }
+}
+
