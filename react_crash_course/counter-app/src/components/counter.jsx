8358e5ba1b4d2ecf970e4bf280f48bc5342e5dd3
John Doan <doanja@plu.edu>
2019-09-03 13:00:23 -0700

    testing react counter

---
+++ react_crash_course/counter-app/src/components/counter.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  state = {
+    count: 1,
+    tags: ['tag1', 'tag2', 'tag3']
+  };
+
+  styles = {
+    fontSize: 50,
+    fontWeight: 'bold'
+  };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.tags.length === 0 && 'pleaes create a new tag'}
+        {this.renderTags()}
+      </div>
+    );
+  }
+}
+
+export default Counter;
