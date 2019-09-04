9d3064c3a53d1d9ed31713636ce201cd75abd951
John Doan <doanja@plu.edu>
2019-09-04 11:31:10 -0700

    react counter example 2

--- react_crash_course/counter-app/src/components/counter.jsx
+++ react_crash_course/counter-app/src/components/counter.jsx
@@ -2,33 +2,36 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 1,
-    tags: ['tag1', 'tag2', 'tag3']
+    count: 0
   };
 
-  styles = {
-    fontSize: 50,
-    fontWeight: 'bold'
+  handleIncrement = product => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
   };
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
 
   render() {
     return (
       <div>
-        {this.state.tags.length === 0 && 'pleaes create a new tag'}
-        {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement({ id: 1 })}
+          className='btn btn-secondary btn-sm'>
+          Increment
+        </button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = 'badge m-2 badge-';
+    classes += this.state.count === 0 ? 'warning' : 'primary';
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'zero' : count;
   }
 }
 
