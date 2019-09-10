428823623bb0f22a6d77ed991c92bca387f6e861
John <spandexerjd@gmail.com>
2019-09-10 13:42:07 -0700

    counter app 4

--- react_crash_course/counter-app/src/components/counter.jsx
+++ react_crash_course/counter-app/src/components/counter.jsx
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = {
-    value: this.props.value
-  };
-
-  handleIncrement = product => {
-    // console.log(product);
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
-    // console.log('props', this.props);
-
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className='btn btn-secondary btn-sm'>
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)} // 1:38:55, calls the delete function in the parent component
+          className='btn btn-danger btn-sm m-2'>
+          Delete
         </button>
       </div>
     );
@@ -27,12 +21,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = 'badge m-2 badge-';
-    classes += this.state.value === 0 ? 'warning' : 'primary';
+    classes += this.props.counter.value === 0 ? 'warning' : 'primary';
     return classes;
   }
 
   formatCount() {
-    const { value: count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? 'zero' : count;
   }
 }
--- react_crash_course/counter-app/src/components/counters.jsx
+++ react_crash_course/counter-app/src/components/counters.jsx
@@ -2,20 +2,22 @@ import React, { Component } from 'react';
 import Counter from './counter';
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
-  };
-
   render() {
+    const { onReset, counters, onDelete, onIncrement } = this.props;
+
     return (
       <div>
-        {this.state.counters.map(counter => (
-          <Counter key={counter.id} value={counter.value}></Counter>
+        <button onClick={onReset} className='btn btn-primary btn-sm m-2'>
+          Reset
+        </button>
+        {counters.map(counter => (
+          <Counter
+            key={counter.id} // reason why counter.id is used twice, is because this is privated by react
+            onDelete={onDelete}
+            onIncrement={onIncrement}
+            // value={counter.value}
+            // id={counter.id}
+            counter={counter}></Counter>
         ))}
       </div>
     );
