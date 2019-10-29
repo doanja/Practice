04ccc9f30722bf01ee6675fed8b1d304206a364e
John Doan <doanja@plu.edu>
2019-10-29 09:21:52 -0700

    cleaned up folders

---
+++ crash-courses/react_crash_todo/src/components/AddTodo.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class AddTodo extends Component {
+  state = {
+    title: ''
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.addTodo(this.state.title);
+    this.setState({ title: ' ' });
+  };
+
+  onChange = e => this.setState({ title: e.target.value });
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit} style={{ display: 'flex' }}>
+        <input type='text' style={{ flex: '10', padding: '5px' }} placeholder={this.state.title} onChange={this.onChange}></input>
+
+        <input type='submit' value='Submit' className='btn' style={{ flex: '1' }}></input>
+      </form>
+    );
+  }
+}
+
+// PropTypes
+AddTodo.propTypes = {
+  addTodo: PropTypes.func.isRequired
+};
+
+export default AddTodo;
