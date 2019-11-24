680b862120ea00afdde85d7f59a2b19889bc099a
John <doanja@plu.edu>
2019-11-23 19:42:10 -0800

    finished material ui form

---
+++ crash-courses/react_step_form_material_ui/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import UserForm from "./components/UserForm";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <UserForm />
+      </div>
+    );
+  }
+}
