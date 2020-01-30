91ef33d221856da501d70e099bcae7f3f19c59f2
John Doan <doanja@plu.edu>
2020-01-29 21:00:57 -0800

    added hook reducers

--- crash courses/hooks - useReducer/src/App.js
+++ crash courses/hooks - useReducer/src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 import CounterOne from './components/CounterOne';
 import CounterTwo from './components/CounterTwo';
 import CounterThree from './components/CounterThree';
+import HookTimer from './components/HookTimer';
 
 function App() {
   return (
     <div className='App'>
-      <CounterThree />
+      <HookTimer />
     </div>
   );
 }
