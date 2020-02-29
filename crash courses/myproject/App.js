80237c4ccdcd33fc8b4d39168a7287d3003de112
John Doan <doanja@plu.edu>
2020-02-28 21:46:58 -0800

    updated with flatlists

--- crash courses/myproject/App.js
+++ crash courses/myproject/App.js
@@ -1,19 +1,80 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('John');
+  const [age, setAge] = useState('10');
+  const [people, setPeople] = useState([
+    { name: 'shan', key: '1' },
+    { name: 'fred', key: '2' },
+    { name: 'bob', key: '3' },
+    { name: 'lisa', key: '4' },
+    { name: 'lisa', key: '6' },
+    { name: 'lisa', key: '7' },
+    { name: 'lisa', key: '8' }
+  ]);
+
+  const clickHandler = () => {
+    setName('Maki');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Chicken and Watermelon</Text>
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Enter name:</Text>
+    //   <TextInput
+    //     style={styles.input}
+    //     placeholder='e.g. John Doe'
+    //     onChangeText={val => setName(val)}
+    //   />
+    //   <Text>Enter age:</Text>
+    //   <TextInput
+    //     keyboardType='numeric'
+    //     style={styles.input}
+    //     placeholder='e.g. 10'
+    //     onChangeText={val => setAge(val)}
+    //   />
+    //   <Text>
+    //     My name is {name}, I am {age} years old.
+    //   </Text>
+
+    // </View>
+    // <ScrollView>
+    //   {people.map(item => (
+    //     <View key={item.key}>
+    //       <Text style={styles.item}>{item.name}</Text>
+    //     </View>
+    //   ))}
+    // </ScrollView>
+
+    <FlatList
+      data={people}
+      renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#fff',
+  //   alignItems: 'center',
+  //   justifyContent: 'center'
+  // },
+  // buttonContainer: {
+  //   marginTop: 20
+  // },
+  // input: {
+  //   borderWidth: 1,
+  //   borderColor: '#777',
+  //   padding: 8,
+  //   margin: 10,
+  //   width: 200
+  // },
+  item: {
+    marginTop: 24,
+    margin: 10,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24
   }
 });
