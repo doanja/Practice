9d1f20d61849ca1c0cc44583457b2f3f7b65598a
John Doan <doanja@plu.edu>
2020-03-02 20:59:38 -0800

    added global styles

---
+++ crash courses/NativeReviewsApp/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/Home';
+import { AppLoading } from 'expo';
+
+const getFonts = () =>
+  Font.loadAsync({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+  });
+
+export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded) {
+    return <Home />;
+  } else {
+    return <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
