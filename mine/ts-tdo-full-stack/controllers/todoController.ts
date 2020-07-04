a46c702d94161f45c1f4f6d8cd837effca5ae34e
John Doan <doanja@plu.edu>
2020-07-03 18:10:34 -0700

    adding controllers for todo

--- mine/ts-tdo-full-stack/controllers/todoController.ts
+++ mine/ts-tdo-full-stack/controllers/todoController.ts
@@ -1,10 +1,14 @@
 import db from '../models';
-import { Request, Response } from 'express';
 
 // define methods for todoController
 export default {
-  findAll: (req: Request, res: Response) =>
-    db.Todo.find(req.query)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err)),
+  findAll: (req: Request, res: Response) => {
+    const todos = db.Todo.find((err: any, todos: any) => {
+      if (err) {
+        res.status(422).json(err);
+      } else {
+        res.json(todos);
+      }
+    });
+  },
 };
