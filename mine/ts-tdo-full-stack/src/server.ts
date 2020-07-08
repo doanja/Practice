269496f7ca3b5ad7fb8d93aec2458d2dcc207bfb
John Doan <doanja@plu.edu>
2020-07-08 15:12:26 -0700

    removed debug code from server.ts and removed Mongo_Info declaration

--- mine/ts-tdo-full-stack/src/server.ts
+++ mine/ts-tdo-full-stack/src/server.ts
@@ -1,11 +1,8 @@
 require('dotenv').config();
 import App from './app';
 import { UserController, TodoController } from '../controllers/';
-console.log('UserController', UserController);
-console.log('TodoController', TodoController);
+
 const { NODE_ENV, MONGODB_URI, MONGODB_USER, MONGODB_PASSWORD, MONGODB_PATH } = process.env;
-const MONGO_INFO = { MONGODB_URI, MONGODB_USER, MONGODB_PASSWORD, MONGODB_PATH };
 
-const app = new App(3000, MONGO_INFO, [new TodoController(), new UserController()], NODE_ENV);
-
+const app = new App(3000, { MONGODB_URI, MONGODB_USER, MONGODB_PASSWORD, MONGODB_PATH }, [new TodoController(), new UserController()], NODE_ENV);
 app.listen();
