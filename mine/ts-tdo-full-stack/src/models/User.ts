a709a3dd2af8251a221c00be802f9beebc56a991
doanja <doanja@plu.edu>
2020-07-23 14:28:51 -0700

    added timestamps to user and todo model

--- mine/ts-tdo-full-stack/src/models/User.ts
+++ mine/ts-tdo-full-stack/src/models/User.ts
@@ -1,18 +1,21 @@
 import { Schema, model } from 'mongoose';
 import { IUser } from '../types/user';
 
-const UserSchema: Schema = new Schema({
-  email: {
-    type: String,
-    trim: true,
-    unique: true,
-    required: true,
+const UserSchema: Schema = new Schema(
+  {
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 export default model<IUser>('User', UserSchema);
