93cfe1fd3186a453d347cf94c81e8c2898fd2141
doanja <doanja@plu.edu>
2020-07-29 16:17:34 -0700

    adding persistance to load state from localstorage

--- mine/ts-tdo-full-stack/client/src/redux/Store.tsx
+++ mine/ts-tdo-full-stack/client/src/redux/Store.tsx
@@ -2,8 +2,42 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/rootReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import axios from 'axios';
 
-const Store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+function saveToLocalStorage(store: any) {
+  try {
+    const serializedStore = JSON.stringify(store);
+    localStorage.setItem('store', serializedStore);
+  } catch (err) {
+    console.log('Error saving to local storage in store:', err);
+  }
+}
+
+function loadFromLocalStorage() {
+  try {
+    const serializedStore = localStorage.getItem('store');
+
+    if (!serializedStore) return undefined;
+
+    const token = JSON.parse(serializedStore).login.token;
+
+    token ? (axios.defaults.headers.common.Authorization = `Bearer ${token}`) : (axios.defaults.headers.common.Authorization = '');
+
+    return JSON.parse(serializedStore);
+  } catch (err) {
+    console.log('Error loading from local storage in store:', err);
+    return undefined;
+  }
+}
+
+const persistedState = loadFromLocalStorage();
+
+const Store = createStore(rootReducer, persistedState, composeWithDevTools(applyMiddleware(thunk)));
+
+Store.subscribe(() => {
+  console.log('Store.getState() :>> ', Store.getState());
+  saveToLocalStorage(Store.getState());
+});
 
 export type RootStore = ReturnType<typeof rootReducer>;
 
