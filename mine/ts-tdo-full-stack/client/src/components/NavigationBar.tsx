32f4e3f98de913d3a73fe33278515ef3ed337372
doanja <doanja@plu.edu>
2020-08-11 15:21:47 -0700

    added logout functionality

--- mine/ts-tdo-full-stack/client/src/components/NavigationBar.tsx
+++ mine/ts-tdo-full-stack/client/src/components/NavigationBar.tsx
@@ -1,22 +1,42 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import { Navbar } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 
+// redux
+import { useSelector, useDispatch } from 'react-redux';
+import { RootStore } from '../redux/Store';
+import { clearAuthToken, clearLoginStatus } from '../redux/actions/authActions';
+
 export const NavigationBar: React.FC = () => {
+  const history = useHistory();
+
+  // redux
+  const { loginStatus } = useSelector((state: RootStore) => state.auth);
+  const dispatch = useDispatch();
+
+  const logout = () => {
+    dispatch(clearAuthToken());
+    dispatch(clearLoginStatus());
+    window.localStorage.removeItem('store');
+  };
+
   return (
     <Navbar bg='dark' variant='dark'>
       <Navbar.Brand href='/'>
         <FontAwesomeIcon icon={faPencilAlt} size='1x' /> Typescript To Do List
       </Navbar.Brand>
-      <Navbar.Collapse className='justify-content-end'>
-        <Navbar.Text>
-          <a href='/' onClick={() => console.log('TODO: delete LS data, and clear axios headers, redirect')}>
-            Logout
-          </a>
-        </Navbar.Text>
-      </Navbar.Collapse>
+      {loginStatus ? (
+        <Navbar.Collapse className='justify-content-end'>
+          <Navbar.Text>
+            <a href='/' onClick={() => logout()}>
+              Logout
+            </a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      ) : null}
     </Navbar>
   );
 };
